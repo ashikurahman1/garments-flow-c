0385e17dcd4186ae9d13fef4d0fbeaad57550be4
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-12-06 20:22:01 +0600

    add: setup firebase and installed dependencies

---
+++ src/shared/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="Garments Flow Logo" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
